b39b062a8f9d964ca66729787e634ff84dd515be
feiron <pawel.rogowski.praca@gmail.com>
2023-12-29 20:57:44 +0100

    modified:   .gitignore

--- node_modules/iohook2/build/Release/.deps/Release/iohook.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/iohook.node := ln -f "Release/obj.target/iohook.node" "Release/iohook.node" 2>/dev/null || (rm -rf "Release/iohook.node" && cp -af "Release/obj.target/iohook.node" "Release/iohook.node")
