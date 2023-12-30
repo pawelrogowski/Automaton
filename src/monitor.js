20040dfad32e018a7153772995cd448b40fd008f
feiron <pawel.rogowski.praca@gmail.com>
2023-12-30 02:50:25 +0100

    fixed multi color rule logic

--- src/monitor.js
+++ src/monitor.js
@@ -3,17 +3,13 @@ const exec = require('child_process').exec;
 
 process.on('message', (rule) => {
   setInterval(() => {
-    rule.colors.forEach((color) => {
-      try {
-        if (color.enabled) {
-          const pixelColor = `#${robotjs.getPixelColor(color.x, color.y)}`;
-          if (pixelColor === color.color) {
-            exec(`xdotool key --window ${rule.windowId} ${rule.key}`);
-          }
-        }
-      } catch (error) {
-        process.send({ error });
-      }
+    const allConditionsMet = rule.colors.every((color) => {
+      const pixelColor = `#${robotjs.getPixelColor(color.x, color.y)}`;
+      return color.enabled ? pixelColor === color.color : pixelColor !== color.color;
     });
+
+    if (allConditionsMet) {
+      exec(`xdotool key --window ${rule.windowId} ${rule.key}`);
+    }
   }, rule.interval);
 });
