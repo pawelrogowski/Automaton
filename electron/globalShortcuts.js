06ce4d8e82a5d0ff3a044b857f4e613b1f53bbb6
Feiron <pawel.rogowski.praca@gmail.com>
2024-03-12 18:20:46 +0100

    added error handling and console logs

--- electron/globalShortcuts.js
+++ electron/globalShortcuts.js
@@ -3,32 +3,53 @@ import { selectWindow } from './menus/windowSelection.js';
 import { selectActiveWindow } from './menus/windowSelection.js';
 import setGlobalState from './setGlobalState.js';
 import { getMainWindow } from './createMainWindow.js';
+import pkg from 'lodash';
+const { debounce } = pkg;
+
+const debounceTime = 75;
+
+const debouncedSelectActiveWindow = debounce(() => {
+  console.log('Alt+0 shortcut clicked');
+  selectActiveWindow();
+}, debounceTime);
+
+const debouncedSelectWindow = debounce(() => {
+  console.log('Alt+Shift+0 shortcut clicked');
+  selectWindow();
+}, debounceTime);
+
+const debouncedToggleBotEnabled = debounce(() => {
+  console.log('Alt+1 shortcut clicked');
+  setGlobalState('global/toggleBotEnabled');
+}, debounceTime);
+
+const debouncedToggleMainWindowVisibility = debounce(() => {
+  console.log('Alt+2 shortcut clicked');
+  const mainWindow = getMainWindow();
+  if (mainWindow) {
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
+    } else {
+      mainWindow.show();
+    }
+  }
+}, debounceTime);
 
 export const registerGlobalShortcuts = () => {
-  globalShortcut.register('Alt+0', () => {
-    selectActiveWindow();
-  });
-
-  globalShortcut.register('Alt+Shift+0', () => {
-    selectWindow();
-  });
-
-  globalShortcut.register('Alt+1', () => {
-    setGlobalState('global/toggleBotEnabled');
-  });
-
-  globalShortcut.register('Alt+2', () => {
-    const mainWindow = getMainWindow();
-    if (mainWindow) {
-      if (mainWindow.isVisible()) {
-        mainWindow.hide();
-      } else {
-        mainWindow.show();
-      }
-    }
-  });
+  try {
+    globalShortcut.register('Alt+0', debouncedSelectActiveWindow);
+    globalShortcut.register('Alt+Shift+0', debouncedSelectWindow);
+    globalShortcut.register('Alt+1', debouncedToggleBotEnabled);
+    globalShortcut.register('Alt+2', debouncedToggleMainWindowVisibility);
+  } catch (error) {
+    console.error('Failed to register global shortcuts:', error);
+  }
 };
 
 export const unregisterGlobalShortcuts = () => {
-  globalShortcut.unregisterAll();
+  try {
+    globalShortcut.unregisterAll();
+  } catch (error) {
+    console.error('Failed to unregister global shortcuts:', error);
+  }
 };
